e609ba27ab3ce2c5c6965ddc2634ef0f0c652cd1
Darya Dobysh <d.bakinovskaya@gmail.com>
2023-06-29 14:57:32 +0400

    feat: added header and refactored folder structure

--- script/burger_menu.js
+++
@@ -1,8 +0,0 @@
-const dom = {
-    burger: document.querySelector('#burger__menu'),
-};
-
-function clickBurger(evt) {
-    dom.burger.classList.toggle('active__menu');
-}
-dom.burger.addEventListener('click', clickBurger);
