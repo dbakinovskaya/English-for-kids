bbfe745d2d2ac3dbf8116da28b2aebb63cad44ce
Darya Dobysh <d.bakinovskaya@gmail.com>
2023-07-02 21:00:22 +0400

    feat: add start button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,7 +10,8 @@ export const dom = {
   body: document.querySelector("body"),
   burger: document.querySelector("#burger__menu"),
   menu: document.querySelector("#menu__wrapper"),
-  mode: document.querySelector(".mode"),
+  mode: document.querySelector(".mode__wrapper"),
+  modeButton: document.querySelector(".mode"),
   menuList: document.querySelector("ul"),
   cardWrapper: document.querySelector(".card__wrapper"),
 };
@@ -19,6 +20,6 @@ dom.menu.addEventListener("click", closeBurgerMenu);
 
 dom.burger.addEventListener("click", showBurgerMenu);
 
-dom.mode.addEventListener("click", clickModeButton);
+dom.modeButton.addEventListener("click", clickModeButton);
 
 getData();
